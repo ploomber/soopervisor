685d954af8d8668adc6bfb9419e0b0adb1c2bf20
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-07-29 23:13:50 -0400

    adding simplest case for running the testing script in a tmp directory

--- src/ploomberci/script.py
+++ src/ploomberci/script.py
@@ -1,3 +1,63 @@
 """
-Generate a script to execute a pipeline
+Command line interface
+
+Steps:
+
+1. Copy project_root to workspace
+2. Look for a conda's environment.yml file, install dependencies (assume conda is installed)
+3. Run `ploomber pipeline pipeline.yaml` (assume al output is saved in ./output)
+4. Install this package
+5. Upload 'output/ to box' (read credentials from ~/.box)
+
+Notes:
+
+Use jinja to generate the script (it makes easy to embed logic)
 """
+from pathlib import Path
+from jinja2 import Template
+
+template = Template("""
+# exit if any command fails
+set -e
+
+# initialize conda in the shell process
+eval "$(conda shell.bash hook)"
+conda activate base
+
+# move to the project_root
+cd {{project_root}}
+
+# install the env, the user might name it differently so we force it to be
+# installed as "ploomber-env"
+conda env create --file environment.yml --name ploomber-env --force
+conda activate ploomber-env
+
+# run pipeline
+ploomber build
+
+{% if box %}
+# command to upload a folder to box...
+ploomberci 
+{% endif %}
+
+""")
+
+
+def generate_script(project_root):
+    """
+    Generate a bash script (string) to run a pipeline in a clean environment
+
+    Parameters
+    ----------
+    project_root : str
+        Path to the project root (the one that contains the pipeline.yaml file)
+    """
+    # User should be able to configure behavior with a ploomber-ci.yaml file
+    config = Path(project_root, 'ploomber-ci.yaml')
+    # read config...
+
+    # example ploomber-ci.yaml
+    """
+    box: true
+    """
+    return template.render(project_root=project_root, box=False)
---
+++ tests/conftest.py
@@ -0,0 +1,37 @@
+import os
+import tempfile
+import shutil
+from pathlib import Path
+
+import pytest
+
+
+def _path_to_tests():
+    return Path(__file__).absolute().parent
+
+
+@pytest.fixture()
+def tmp_directory():
+    old = os.getcwd()
+    tmp = tempfile.mkdtemp()
+    os.chdir(str(tmp))
+
+    yield tmp
+
+    shutil.rmtree(str(tmp))
+    os.chdir(old)
+
+
+@pytest.fixture()
+def tmp_sample_project():
+    old = os.getcwd()
+    tmp = Path(tempfile.mkdtemp(), 'sample_project')
+    sample_project = _path_to_tests() / 'assets' / 'sample_project'
+    shutil.copytree(str(sample_project), str(tmp))
+
+    os.chdir(str(tmp))
+
+    yield tmp
+
+    shutil.rmtree(str(tmp))
+    os.chdir(old)
