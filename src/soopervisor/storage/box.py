780166c55b573fb92d8927316e46f1506b89b2b9
Lety <leticia.cruz546@gmail.com>
2020-08-13 14:15:48 -0500

    Refactor: Change tests for the new Box structure

--- src/soopervisor/storage/box.py
+++ src/soopervisor/storage/box.py
@@ -5,6 +5,7 @@ import hashlib
 import logging
 from pathlib import Path
 from typing import List
+from tqdm import tqdm
 
 import yaml
 from boxsdk import Client, OAuth2
@@ -19,7 +20,7 @@ class BoxUploader:
     Examples
     --------
     >>> from soopervisor.storage.box import BoxUploader
-    >>> uploader = BoxUploader('~/.auth/box.yaml')
+    >>> uploader = BoxUploader.from_yaml('~/.auth/box.yaml')
     >>> uploader.upload_files(['some_file.png'])
     """
     def __init__(self, client_id, primary_access_token):
@@ -30,6 +31,7 @@ class BoxUploader:
         )
 
         self.client = Client(auth)
+        self.__root_folder = self.client.root_folder().get()
 
     @classmethod
     def from_yaml(cls, path_to_credentials):
@@ -42,7 +44,10 @@ class BoxUploader:
     def from_environ(cls):
         return cls(os.environ['client_id'], os.environ['primary_access_token'])
 
-    def upload_files(self, paths: List[str], replace: bool = False):
+    def get_root_folder(self):
+        return self.__root_folder
+
+    def upload_files(self, paths: List[str], replace: bool = False, root_folder = None):
         """
         Upload files to Box
 
@@ -53,19 +58,24 @@ class BoxUploader:
         replace: bool,optional
             If True, all the files and folders will be replaced by the new ones
         """
-        root_folder = self.client.root_folder().get()
+        if not root_folder:
+            root_folder = self.__root_folder
 
         for path in paths:
             path = Path(path)
             if path.exists():
                 if path.is_dir():
+                    total_size_upload = sum(file.stat().st_size for file in path.glob('**/*') if (not file.name.startswith(".") and file.is_file()))
+                    progress_bar = tqdm(total=total_size_upload)
+                    folder = self._create_folder(root_folder.id, path.name, replace_folder=replace)
                     self._upload_directory(path=path,
-                                           parent_folder_id=root_folder.id,
-                                           replace_folder=replace)
+                                            parent_folder_id=folder.id,
+                                            progress_bar=progress_bar)
                 elif path.is_file():
                     self._upload_each_file(folder_id=root_folder.id,
-                                           file=path,
-                                           replace_file=replace)
+                                        file=path,
+                                        progress_bar=progress_bar,
+                                        replace_file=replace)
             else:
                 raise FileNotFoundError("Invaid or non-existent path")
 
@@ -74,6 +84,7 @@ class BoxUploader:
     def _upload_directory(self,
                           path: Path,
                           parent_folder_id: str,
+                          progress_bar,
                           replace_folder: bool = False):
         """
         Upload a directory
@@ -84,6 +95,8 @@ class BoxUploader:
             The path to be uploaded
         parent_folder_id:
             The folder id from Box where the directory is to be uploaded
+        progress_bar:
+            Used to show the progress for the uploading folders
         replace_folder: bool,optional
             If True, the folder on Box is to be replaced by the one to be
             uploaded
@@ -93,16 +106,17 @@ class BoxUploader:
         All the .* files will be ignored
 
         """
-        folder_id = self._create_folder(parent_folder_id, path.name,
-                                        replace_folder)
-
-        file_system_objects = (file for file in path.glob('**/*')
-                               if not file.name.startswith("."))
+        file_system_objects = (file for file in path.iterdir()
+                            if not file.name.startswith("."))
         for object in file_system_objects:
             if object.is_dir():
-                folder_id = self._create_folder(folder_id, object.name)
+                folder = self._create_folder(parent_folder_id, object.name)
+                self._upload_directory(path=object.absolute(), parent_folder_id=folder.id, progress_bar=progress_bar)
             elif object.is_file():
-                self._upload_each_file(folder_id=folder_id, file=object)
+                self._upload_each_file(folder_id=parent_folder_id, file=object)
+
+            progress_bar.set_postfix(file=object.name, refresh=False)
+            progress_bar.update(object.stat().st_size)
 
         return True
 
@@ -114,7 +128,7 @@ class BoxUploader:
             subfolder = self.client.folder(
                 folder_id=parent_folder_id).create_subfolder(folder_name)
             logging.info("Folder {} created".format(folder_name))
-            return subfolder.id
+            return subfolder
         except BoxAPIException as e:
             if replace_folder:
                 folder_id = e.context_info.get("conflicts")[0].get("id")
@@ -126,8 +140,8 @@ class BoxUploader:
     def _delete_folder(self, folder_id: str):
         try:
             return self.client.folder(folder_id).delete()
-        except BoxValueError:
-            raise ValueError("Invalid file ID")
+        except BoxAPIException:
+            raise ValueError("Invalid folder ID")
 
     def _upload_each_file(self,
                           folder_id: str,
@@ -135,12 +149,13 @@ class BoxUploader:
                           replace_file: bool = False):
         file_size = file.stat().st_size
         if file_size < CHUNKED_UPLOAD_MINIMUM:
-            response = self._upload_file(folder_id, file, replace_file)
+            file_uploaded = self._upload_file(folder_id, file, replace_file)
         else:
-            response = self._upload_large_file(folder_id, file.name, file_size,
-                                               replace_file)
+            file_uploaded = self._upload_large_file(folder_id=folder_id, file=file, file_size=file_size,
+                                               replace_file=replace_file)
 
-        return response
+
+        return file_uploaded
 
     def _upload_file(self,
                      folder_id: str,
@@ -148,8 +163,8 @@ class BoxUploader:
                      replace_file: bool = False):
         try:
             new_file = self.client.folder(folder_id=folder_id).upload(file)
-            logging.info("{} uploaded".format(new_file.name))
-            return True
+            logging.info("File {} uploaded".format(new_file.name))
+            return new_file
         except BoxAPIException as e:
             context_info = e.context_info
             file_id = context_info.get("conflicts").get("id")
@@ -162,41 +177,45 @@ class BoxUploader:
     def _delete_file(self, file_id: str):
         try:
             return self.client.file(file_id).delete()
-        except BoxValueError:
+        except BoxAPIException:
             raise ValueError("Invalid file ID")
 
     def _upload_large_file(self,
                            folder_id: str,
-                           file_name: str,
+                           file: Path,
                            file_size: int,
                            replace_file: bool = False):
         part_array = []
-        with open(file_name, mode="rb") as f:
-            sha1 = hashlib.sha1()
-            upload_session = self.client.folder(
-                folder_id=folder_id).create_upload_session(file_size,
-                                                           file_name=file_name)
+        with tqdm(total=file_size, unit="B", desc=file.name) as progress_bar:
+            with open(file.absolute(), mode="rb") as f:
+                sha1 = hashlib.sha1()
+                upload_session = self.client.folder(
+                    folder_id=folder_id).create_upload_session(file_size,
+                                                            file_name=file.name)
 
-            for part_num in range(upload_session.total_parts):
-                copied_length = 0
-                chunk = b''
-                while copied_length < upload_session.part_size:
-                    bytes_read = f.read(upload_session.part_size -
-                                        copied_length)
-                    if bytes_read is None:
-                        continue
-                    if len(bytes_read) == 0:
-                        break
-                    chunk += bytes_read
-                    copied_length += len(bytes_read)
+                for part_num in range(upload_session.total_parts):
+                    copied_length = 0
+                    chunk = b''
+                    while copied_length < upload_session.part_size:
+                        bytes_read = f.read(upload_session.part_size -
+                                            copied_length)
+                        if bytes_read is None:
+                            continue
+                        if len(bytes_read) == 0:
+                            break
+                        chunk += bytes_read
+                        copied_length += len(bytes_read)
 
-                uploaded_part = upload_session.upload_part_bytes(
-                    chunk, part_num * upload_session.part_size, file_size)
-                part_array.append(uploaded_part)
-                updated_sha1 = sha1.update(chunk)
-            content_sha1 = sha1.digest()
-            uploaded_file = upload_session.commit(content_sha1=content_sha1,
-                                                  parts=part_array)
-            logging.info("File: {0} uploaded".format(uploaded_file.name))
+                        progress_bar.update(copied_length)
+
+                    uploaded_part = upload_session.upload_part_bytes(
+                        chunk, part_num * upload_session.part_size, file_size)
+                    part_array.append(uploaded_part)
+                    updated_sha1 = sha1.update(chunk)
+                content_sha1 = sha1.digest()
+                uploaded_file = upload_session.commit(content_sha1=content_sha1,
+                                                    parts=part_array)
+
+        logging.info("File: {0} uploaded".format(uploaded_file.name))
 
         return True
---
+++ tests/test_box_storage.py
@@ -0,0 +1,185 @@
+import pytest
+from boxsdk.exception import BoxValueError, BoxAPIException
+from pathlib import Path
+from tqdm import tqdm
+
+from soopervisor.storage.box import BoxUploader, CHUNKED_UPLOAD_MINIMUM
+
+
+PATH_CREDENTIALS="~/.box/credentials.yaml"
+
+def test_upload_files(tmpdir):
+    tmp_dir = tmpdir.mkdir("tmp_ploomber_folder")
+    file1 = tmp_dir.mkdir("subdir1").join("file1.txt")
+    file1.write("lorem ipsum")
+    file2 = tmp_dir.mkdir("subdir2").join("file2.txt")
+    file2.write("lorem ipsum")
+
+    box = BoxUploader.from_yaml(PATH_CREDENTIALS)
+    root_folder = box.get_root_folder()
+    tmp_directory_tests = box._create_folder(parent_folder_id=root_folder.id, folder_name="tmp_directory_tests")
+
+    assert box.upload_files([tmp_dir.strpath], replace=True, root_folder=tmp_directory_tests)
+    assert box._delete_folder(folder_id=tmp_directory_tests.id)
+
+
+def test_upload_files_wrong_paths():
+    box = BoxUploader.from_yaml(PATH_CREDENTIALS)
+    with pytest.raises(FileNotFoundError):
+        assert box.upload_files(["wrong/path"])
+
+
+def test_upload_directory(tmpdir):
+    tmp_dir = tmpdir.mkdir("tmp_ploomber_folder")
+
+    file1 = tmp_dir.mkdir("subdir1").join("file1.txt")
+    file1.write("lorem ipsum")
+    file2 = tmp_dir.mkdir("subdir2").join("file2.txt")
+    file2.write("lorem ipsum")
+
+    box = BoxUploader.from_yaml(PATH_CREDENTIALS)
+    root_folder = box.get_root_folder()
+    tmp_directory_tests = box._create_folder(parent_folder_id=root_folder.id, folder_name="tmp_directory_tests")
+
+    progress_bar = tqdm(total=file1.size() + file2.size())
+
+    response = box._upload_directory(path=Path(tmp_dir.strpath),
+                                 parent_folder_id=tmp_directory_tests.id,
+                                 progress_bar=progress_bar,
+                                 replace_folder=True)
+
+    assert box._delete_folder(folder_id=tmp_directory_tests.id)
+
+
+def test_create_folder():
+    box = BoxUploader.from_yaml(PATH_CREDENTIALS)
+    root_folder = box.get_root_folder()
+    assert box._create_folder(root_folder.id, "tmp_folder")
+
+    with pytest.raises(FileExistsError):
+        assert box._create_folder(root_folder.id, "tmp_folder")
+
+    folder = box._create_folder(parent_folder_id=root_folder.id, folder_name="tmp_folder", replace_folder=True)
+
+    assert box._delete_folder(folder.id)
+
+
+def test_delete_folder():
+    box = BoxUploader.from_yaml(PATH_CREDENTIALS)
+    root_folder = box.get_root_folder()
+    folder = box._create_folder(root_folder.id, "tmp_folder")
+    assert box._delete_folder(folder.id)
+
+    with pytest.raises(ValueError):
+        assert box._delete_folder(folder.id)
+
+
+def test_upload_each_file_normal_size(tmp_path):
+    file = tmp_path / "tmp_ploomber_file.txt"
+    file.write_text("lorem ipsum")
+
+    box = BoxUploader.from_yaml(PATH_CREDENTIALS)
+    root_folder = box.get_root_folder()
+
+    file_box = box._upload_each_file(folder_id=root_folder.id, file=Path(file.absolute()))
+    assert file_box
+
+    assert box._delete_file(file_box.id)
+
+
+def test_upload_each_file_large_size(tmp_path, faker, monkeypatch):
+    class Stat:
+        def __init__(self):
+            self.st_size = CHUNKED_UPLOAD_MINIMUM + 1
+
+    def mock_file():
+        class MockStat:
+            def __init__(self):
+                self.name = "file.txt"
+
+            @staticmethod
+            def stat():
+                return Stat()
+
+        return MockStat()
+
+    def mock_upload_large_file(*args, **kwargs):
+        return True
+
+    box = BoxUploader.from_yaml(PATH_CREDENTIALS)
+    monkeypatch.setattr(box, "_upload_large_file", mock_upload_large_file)
+
+    assert box._upload_each_file(folder_id=box.get_root_folder().id, file=mock_file())
+
+
+def test_upload_file(tmp_path):
+    file = tmp_path / "tmp_ploomber_file.txt"
+    file.write_text("lorem ipsum")
+
+    box = BoxUploader.from_yaml(PATH_CREDENTIALS)
+    root_folder = box.get_root_folder()
+    file_box = box._upload_file(folder_id=root_folder.id, file=file.absolute())
+    assert file_box
+
+    with pytest.raises(FileExistsError):
+        box._upload_file(folder_id=root_folder.id, file=file.absolute())
+
+    file_box = box._upload_file(folder_id=root_folder.id, file=file.absolute(), replace_file=True)
+    assert file_box
+
+    # Remove the file from Box account used by the test
+    assert box._delete_file(file_box.id)
+
+
+def test_delete_file(tmp_path):
+    file = tmp_path / "tmp_ploomber_file.txt"
+    file.write_text("lorem ipsum")
+    box = BoxUploader.from_yaml(PATH_CREDENTIALS)
+    root_folder = box.get_root_folder()
+    file_box = box._upload_file(folder_id=root_folder.id, file=file.absolute())
+    assert box._delete_file(file_box.id)
+
+    with pytest.raises(ValueError):
+        box._delete_file(file_box.id)
+
+
+def test_upload_large_file(faker, monkeypatch, tmp_path):
+    class File:
+        def __init__(self):
+            self.name = "file.txt"
+
+    class UploadSession:
+        def __init__(self):
+            self.part_size = faker.pyint(min_value=1, max_value=100)
+            self.total_parts = 1
+
+        def upload_part_bytes(*args, **kwargs):
+            return None
+
+        def commit(*args, **kwargs):
+            return File()
+
+    def mock_folder(folder_id):
+        class MockFolder:
+            @staticmethod
+            def create_upload_session(*args, **kwargs):
+                return UploadSession()
+
+        return MockFolder()
+
+    box = BoxUploader.from_yaml(PATH_CREDENTIALS)
+    monkeypatch.setattr(box.client, "folder", mock_folder)
+
+    file = tmp_path / "tmp_ploomber_file.txt"
+    file.write_text("lorem ipsum")
+
+    result = box._upload_large_file(folder_id=str(faker.pyint()), file=file,
+                                    file_size=faker.pyint())
+    assert result
+
+
+def test_upload_large_file_not_found(faker):
+    box = BoxUploader.from_yaml(PATH_CREDENTIALS)
+    with pytest.raises(FileNotFoundError):
+        assert box._upload_large_file(folder_id=str(faker.pyint()), file=Path("file.txt"),
+                                      file_size=faker.pyint())
