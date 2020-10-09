490311741648068a9bf06230e2ff021fb09c29b4
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-10-08 23:04:39 -0400

    fixes setup.py

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     ],
     install_requires=REQUIRES,
     extras_require={
-        'dev': EXTRAS_DEV,
+        'dev': EXTRAS_OPT + EXTRAS_DEV,
         'opt': EXTRAS_OPT
     },
     setup_requires=[],
