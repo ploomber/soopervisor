26ece37b68e48aea75839c75b7eebdf9a5809158
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-10-08 23:56:11 -0400

    adds missing arg

--- tests/test_build.py
+++ tests/test_build.py
@@ -6,11 +6,11 @@ from soopervisor.executors.LocalExecutor import LocalExecutor
 
 
 def test_build(git_hash, tmp_sample_project):
-    build.build_project('.', clean_products_path=False)
+    build.build_project('.', clean_products_path=False, dry_run=False)
 
 
 def test_clean_products_path(git_hash, monkeypatch, tmp_sample_project):
     monkeypatch.setattr(LocalExecutor, 'execute', lambda *args: None)
-    build.build_project('.', clean_products_path=True)
+    build.build_project('.', clean_products_path=True, dry_run=False)
 
     assert not len(os.listdir('output')) and Path('output').is_dir()
