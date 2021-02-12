a8d10d8d9beb836c6815a46706eeedded1142aeb
Eduardo Blancas Reyes <github@blancas.io>
2021-02-11 19:22:43 -0500

    merging files that test airflow export

--- tests/test_export_airflow.py
+++ tests/test_export_airflow.py
@@ -1,7 +1,10 @@
 import os
 import subprocess
 from pathlib import Path
+import importlib
 
+from airflow import DAG
+from airflow.operators.bash import BashOperator
 import pytest
 
 from soopervisor.airflow import export
@@ -23,5 +26,62 @@ def tmp_projects(tmpdir_factory):
     ['ml-intermediate', 'etl'],
 )
 def test_generate_valid_airflow_dags(name, tmp_projects):
-    export.project(project_root=f'projects/{name}', output_path='.')
+    subprocess.run([
+        'soopervisor',
+        'export-airflow',
+        '--root',
+        f'projects/{name}',
+        '--output',
+        '.',
+    ],
+                   check=True)
     subprocess.run(['python', f'dags/{name}.py'], check=True)
+
+
+def test_export_airflow(monkeypatch, tmp_sample_project):
+    export.project(project_root='.', output_path='exported')
+    monkeypatch.syspath_prepend('exported/dags')
+    airflow_home = Path(tmp_sample_project, 'exported').resolve()
+    monkeypatch.setenv('AIRFLOW_HOME', airflow_home)
+    mod = importlib.import_module('sample_project')
+    dag = mod.dag
+
+    assert isinstance(dag, DAG)
+    assert set(dag.task_dict) == {'clean', 'plot', 'raw'}
+    assert set(type(t) for t in dag.tasks) == {BashOperator}
+    assert {n: t.upstream_task_ids
+            for n, t in dag.task_dict.items()} == {
+                'raw': set(),
+                'clean': {'raw'},
+                'plot': {'clean'}
+            }
+
+
+def test_export_airflow_callables(monkeypatch, tmp_callables):
+    export.project(project_root='.', output_path='exported')
+    monkeypatch.syspath_prepend('exported/dags')
+    airflow_home = Path(tmp_callables, 'exported').resolve()
+    monkeypatch.setenv('AIRFLOW_HOME', airflow_home)
+    mod = importlib.import_module('callables')
+    dag = mod.dag
+
+    assert isinstance(dag, DAG)
+    assert set(dag.task_dict) == {'features', 'fit.py', 'get', 'join'}
+    assert set(type(t) for t in dag.tasks) == {BashOperator}
+    assert {n: t.upstream_task_ids
+            for n, t in dag.task_dict.items()} == {
+                'get': set(),
+                'features': {'get'},
+                'join': {'features', 'get'},
+                'fit.py': {'join'}
+            }
+
+    # from IPython import embed
+    # embed()
+
+
+def test_export_airflow_no_airflow_env(tmp_callables, capsys):
+    Path('env.airflow.yaml').unlink()
+    export.project(project_root='.', output_path='exported')
+
+    assert 'No env.airflow.yaml found...' in capsys.readouterr().out
