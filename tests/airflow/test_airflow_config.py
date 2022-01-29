13d9b91d7f2249f69bad30feade2eaaa5aa0c7e1
Eduardo Blancas Reyes <github@blancas.io>
2022-01-29 13:04:43 -0600

    deletes unused test file

--- tests/airflow/test_airflow_config.py
+++
@@ -1,9 +0,0 @@
-from soopervisor.airflow.config import AirflowConfig
-
-
-def test_default_values(session_sample_project):
-    config = AirflowConfig.hints()
-    assert config == {
-        'backend': 'airflow',
-        'repository': 'your-repository/name'
-    }
