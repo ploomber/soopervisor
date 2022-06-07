3ee5dec738c576652b0840704d6f1e37a6678fb2
Eduardo Blancas Reyes <github@blancas.io>
2022-06-07 14:36:50 +0200

    updates references to py 3.6

--- setup.py
+++ setup.py
@@ -103,9 +103,9 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     install_requires=REQUIRES,
     extras_require={
