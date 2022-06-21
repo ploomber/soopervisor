a6f25ada7a297196e77c09e7c3b5f680b861572b
Eduardo Blancas Reyes <github@blancas.io>
2022-06-21 10:39:16 +0200

    fixing test

--- src/soopervisor/airflow/export.py
+++ src/soopervisor/airflow/export.py
@@ -95,7 +95,7 @@ class AirflowExporter(abc.AbstractExporter):
                     commons.dependencies.get_default_image_key()]
                 dag_dict = generate_airflow_spec(tasks, args, target_image)
 
-                path_dag_dict_out = Path(env_name, pkg_name + '.json')
+                path_dag_dict_out = Path(pkg_name + '.json')
                 path_dag_dict_out.write_text(json.dumps(dag_dict))
 
             else:
