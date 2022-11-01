75905e69ee1b65494fbfa6d8abe61ce9c71b5a8f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-01 15:16:34 +0000

    Adding tarfile member sanitization to extractall()

--- tests/test_commons.py
+++ tests/test_commons.py
@@ -331,7 +331,26 @@ def test_compress_dir(tmp_empty):
                             'dist/project-name.tar.gz')
 
     with tarfile.open('dist/project-name.tar.gz', 'r:gz') as tar:
-        tar.extractall('.')
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, ".")
 
     expected = {Path('project-name/file')}
     assert set(Path(p) for p in source.glob_all('project-name')) == expected
--- tests/test_dist.py
+++ tests/test_dist.py
@@ -47,7 +47,26 @@ def extract_zip(path, tmp_path):
 
 def extract_tar(path, tmp_path):
     with tarfile.open(path, "r:gz") as tar:
-        tar.extractall(tmp_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, tmp_path)
 
 
 def get_dir_name(tmp_path):
