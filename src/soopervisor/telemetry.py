9bca3193b9c2ee4ade55b94c0f21b574c44427a2
Eduardo Blancas <github@blancas.io>
2023-01-23 18:20:02 -0300

    fixes telemetry

--- src/soopervisor/telemetry.py
+++ src/soopervisor/telemetry.py
@@ -3,4 +3,4 @@ from soopervisor import __version__
 
 POSTHOG_API_KEY = "phc_P9SpSeypyPwxrMdFn2edOOEooQioF2axppyEeDwtMSP"
 
-telemetry = Telemetry(POSTHOG_API_KEY, __version__, "soopervisor")
+telemetry = Telemetry(POSTHOG_API_KEY, "soopervisor", __version__)
